9f3e523e2b986cff97c767f6a3f6e5e32c3042cd
kfirfitousi <kfirp84@gmail.com>
2023-03-08 21:17:27 +0200

    fix: better use of enums

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,30 @@ enum FizzBuzz {
     Number(u32),
 }
 
+impl FizzBuzz {
+    fn new(number: u32) -> Self {
+        match (number % 5 == 0, number % 3 == 0) {
+            (true, true) => FizzBuzz::FizzBuzz,
+            (false, true) => FizzBuzz::Fizz,
+            (true, false) => FizzBuzz::Buzz,
+            (false, false) => FizzBuzz::Number(number),
+        }
+    }
+}
+
+impl Display for FizzBuzz {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            FizzBuzz::Fizz => write!(f, "{}", "Fizz".blue()),
+            FizzBuzz::Buzz => write!(f, "{}", "Buzz".yellow()),
+            FizzBuzz::FizzBuzz => write!(f, "{}{}", "Fizz".blue(), "Buzz".yellow()),
+            FizzBuzz::Number(number) => write!(f, "{}", number),
+        }
+    }
+}
+
 fn fizzbuzz(number: u32) {
-    (1..=number).for_each(|x| println!("{}", <u32 as Into<FizzBuzz>>::into(x)))
+    (1..=number).for_each(|x| println!("{}", FizzBuzz::new(x)))
 }
 
 fn main() {
@@ -38,29 +60,5 @@ fn main() {
         };
 
         return fizzbuzz(number);
-    }
-}
-
-// number --> FizzBuzz
-impl Into<FizzBuzz> for u32 {
-    fn into(self) -> FizzBuzz {
-        match (self % 5, self % 3) {
-            (0, 0) => FizzBuzz::FizzBuzz,
-            (_, 0) => FizzBuzz::Fizz,
-            (0, _) => FizzBuzz::Buzz,
-            (_, _) => FizzBuzz::Number(self),
-        }
-    }
-}
-
-// FizzBuzz --> formatted string
-impl Display for FizzBuzz {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        match self {
-            FizzBuzz::Fizz => write!(f, "{}", "Fizz".blue()),
-            FizzBuzz::Buzz => write!(f, "{}", "Buzz".yellow()),
-            FizzBuzz::FizzBuzz => write!(f, "{}{}", "Fizz".blue(), "Buzz".yellow()),
-            FizzBuzz::Number(number) => write!(f, "{}", number),
-        }
     }
 }
